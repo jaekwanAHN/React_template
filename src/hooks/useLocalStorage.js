606e6c8d12d975637b8e5fe4a903f36a6cb0aef1
JaeKwan Ahn <52008@naver.com>
2022-12-22 16:58:55 +0900

    Feat: use[Local || Session] Storage 구현

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,29 @@
+const { useState } = require("react");
+
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        typeof value === "function" ? value(storedValue) : value;
+
+      setStoredValue(valueToStore);
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useLocalStorage;
