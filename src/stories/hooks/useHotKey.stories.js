9be55fe6ededa8d80e2a21876fc369a9fd5ad83e
JaeKwan Ahn <52008@naver.com>
2022-12-23 15:22:39 +0900

    Feat: useHotKey 구현

---
+++ src/stories/hooks/useHotKey.stories.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import useHotKey from "../../hooks/useHotKey";
+
+export default {
+  title: "Hook/useHotKey",
+};
+
+export const Default = () => {
+  const [value, setValue] = useState("");
+
+  const hotkeys = [
+    {
+      global: true,
+      combo: "meta+shift+k",
+      onKeyDown: (e) => {
+        alert("meta+shift+k");
+      },
+    },
+    {
+      combo: "esc",
+      onKeyDown: (e) => {
+        setValue("");
+      },
+    },
+  ];
+
+  const { handleKeyDown } = useHotKey(hotkeys);
+
+  return (
+    <div>
+      <div>useHotKey Test</div>
+      <div>
+        <input
+          onKeyDown={handleKeyDown}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
